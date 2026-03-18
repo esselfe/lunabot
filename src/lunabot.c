dfb259b4859434898a1250c493a0bda12812ce09
Stephane Fontaine <esselfe16@gmail.com>
2026-03-17 20:26:02 -0400

    0.6.1: version bump

--- src/lunabot.c
+++ src/lunabot.c
@@ -23,7 +23,7 @@
 #include "lunabot.h"
 #include "liblunabot.h"
 
-const char *lunabot_version_string = "0.6.0";
+const char *lunabot_version_string = "0.6.1";
 
 struct GlobalVariables globals, **globals_ptr;
 char buffer[BUFFER_SIZE];
