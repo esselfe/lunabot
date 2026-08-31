c24664df074c2c1749a1890ac87c06afc50b4fd9
Stephane Fontaine <esselfe16@gmail.com>
2026-08-31 08:20:58 -0400

    0.6.4: fix possible lock order reversal warning (SonarCloud)

--- src/lib/irc.c
+++ src/lib/irc.c
@@ -11,13 +11,12 @@ void SendIrcMessage(const char *message) {
 	snprintf(buffer_msg, sizeof(buffer_msg), "PRIVMSG %s :%s\r\n",
 		libglobals->channel, message);
 
-	pthread_mutex_lock(&libglobals->irc_write_mutex);
 	if (libglobals->pSSL == NULL || !libglobals->irc_ready) {
-		pthread_mutex_unlock(&libglobals->irc_write_mutex);
 		Log(LOCAL, "liblunabot::SendIrcMessage() warning: IRC connection is not ready");
 		return;
 	}
 	Log(OUT, (char *)message);
+	pthread_mutex_lock(&libglobals->irc_write_mutex);
 	SSL_write(libglobals->pSSL, buffer_msg, strlen(buffer_msg));
 	pthread_mutex_unlock(&libglobals->irc_write_mutex);
 }
